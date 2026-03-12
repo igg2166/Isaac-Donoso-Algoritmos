352bb38da8ac4c08592cf508149a8194146afcc0
igg2166 <isaac_donoso95221@elpoli.edu.co>
2026-03-12 09:36:41 -0500

    aaaaaa

--- clase 12 de marzo/conjuntos.py
+++ clase 12 de marzo/conjuntos.py
@@ -13,3 +13,8 @@ def eliminar_duplicados_opti(lista):
 
 print(eliminar_duplicados([1,1,1,1,2,3,4,5,6]))
 print(eliminar_duplicados_opti([1,1,1,1,2,3,4,5,6]))
+
+#No se puede usar conjuntos si se necesita trabajar con elementos duplicados
+#No se puede usar conjuntos cuando necesitas tener un orden especifico de datos
+#No olvides los conjuntos no tienen orden
+#Se utilizar las variables add, remove, discard, sorted(este vuelve el connjunto en una lista y lo ordena)
